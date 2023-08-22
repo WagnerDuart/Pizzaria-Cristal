0ad4145923a896ecc1fcbb7e3f1a6f9278286d76
WagnerDuart <124386335+WagnerDuart@users.noreply.github.com>
2023-08-22 19:49:14 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,20 +1,33 @@
-
 function initMap() {
-    const cairo = { lat: -15.941315, lng: -41.859770 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      scaleControl: true,
-      center: cairo,
-      zoom: 10,
-    });
-    const infowindow = new google.maps.InfoWindow();
-  
-    infowindow.setContent("<b>القاهرة</b>");
-  
-    const marker = new google.maps.Marker({ map, position: cairo });
-  
-    marker.addListener("click", () => {
-      infowindow.open(map, marker);
-    });
-  }
-  
-  window.initMap = initMap;
+  const cairo = { lat: -15.941315, lng: -41.85977 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    scaleControl: true,
+    center: cairo,
+    zoom: 10,
+  });
+  const infowindow = new google.maps.InfoWindow();
+
+  infowindow.setContent("<b>القاهرة</b>");
+
+  const marker = new google.maps.Marker({ map, position: cairo });
+
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
+}
+
+window.initMap = initMap;
+
+$(document).ready(function () {
+  $(".scroll-link").on("click", function (event) {
+    event.preventDefault();
+    var target = $(this.hash);
+
+    $("html, body").animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      800
+    );
+  });
+});
